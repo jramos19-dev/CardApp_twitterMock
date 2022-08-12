154f317c550128e4648738c3cb6cfb3067da52a3
Jorge Ramos <jlramoss@yahoo.com.mx>
2022-08-12 11:39:02 -0400

    finished the application added display flex, and justify content and flex-wrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   
   posts = [{title:'trees',imgUrl: 'assets/trees.jpg',userName:'@trees', content:"Saw this cool tree in my hike toda!"  },
   {title:'biking',imgUrl: 'assets/biking.jpg',userName:'@biking', content:"Saw this cool tree in my hike toda!"  },
+  {title:'mountains',imgUrl: 'assets/mountain.jpg',userName:'@nature', content:"Saw this cool tree in my hike toda!" },
   {title:'mountains',imgUrl: 'assets/mountain.jpg',userName:'@nature', content:"Saw this cool tree in my hike toda!" }
 ]
 
